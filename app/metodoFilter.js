0f0c5aec187d173cd8ea3baaf68f84a41a4e0181
Ariom Pimenta <ariompf@gmail.com>
2024-05-30 06:53:20 -0300

    motodoFilter add function forEach Filter btn

---
+++ app/metodoFilter.js
@@ -0,0 +1,15 @@
+//HTML
+/* const btnFiltrarLivrosFront = document.getElementById('btnFiltrarLivrosFront');
+btnFiltrarLivrosFront.addEventListener('click', filtrarLivros); */
+const botoes = document.querySelectorAll('.btn');
+
+//ForEach
+botoes.forEach(btn => btn.addEventListener('click',filtrarLivros))
+
+//Filter
+function filtrarLivros() {
+    const elementoBtn = document.getElementById(this.id)
+    const categoria = elementoBtn.value
+    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
+    console.table(livrosFiltrados)
+}
