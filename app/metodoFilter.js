feb2dba3abf25f011b75389d0ce35ef86867a7d1
Ariom Pimenta <ariompf@gmail.com>
2024-05-31 06:55:51 -0300

    div exibirValorTotal

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -10,15 +10,29 @@ botoes.forEach(btn => btn.addEventListener('click',filtrarLivros));
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id)
     const categoria = elementoBtn.value
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
+    let livrosFiltrados = filtrarPorCategiria(categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
 };
 
 //let livrosFiltrados = botaoFiltro.value == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria == categoria)
 
+function filtrarPorCategiria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria);
+}
+
 //Filtro Disponibilidade funcionando
 document.getElementById('btnLivrosDisponiveis').addEventListener('click', filtrarLivrosDisponiveis);
+
  function filtrarLivrosDisponiveis() {
     const livrosDisponiveis = livros.filter(livro => livro.quantidade > 0);
     exibirOsLivrosNaTela(livrosDisponiveis);
+    exibirComValorTotalDeLivrosDisponiveis()
 };
+
+function exibirComValorTotalDeLivrosDisponiveis() {
+    elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+    <div class="livros__disponiveis">
+    <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+  </div>
+    `
+}
--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,8 +1,10 @@
 //HTML
-const elementoParaInserirLivros = document.getElementById('livros')
+const elementoParaInserirLivros = document.getElementById('livros');
+const elementoComValorTotalDeLivrosDisponiveis = document.getElementById('valor_total_livros_disponiveis');
 
 //Função "forEach"
 function exibirOsLivrosNaTela(listaDeLivros) {
+  elementoComValorTotalDeLivrosDisponiveis.innerHTML = ''
     elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach(livro => {
       //let disponibilidade = verificarDisponibilidadeDoLivro(livro)
